02b9d3a86de6d8b5a739711e7d3a5972f370642f
esjjim <esjjim@SAG-3VB8PC2.eur.ad.sag>
2020-03-30 09:16:46 +0200

    Completed the backend for feature one.

--- springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookCategoryRepository.java
+++ springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookCategoryRepository.java
@@ -3,9 +3,12 @@ package in.bushansirgur.onlinebookstore.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import in.bushansirgur.onlinebookstore.entity.BookCategory;
 
+// * Customizing Spring Data Rest > Configuring the Rest URL Path
+@RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }
