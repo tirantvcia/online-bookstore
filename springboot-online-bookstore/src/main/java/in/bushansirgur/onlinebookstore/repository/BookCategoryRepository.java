416eca07830d5a038d3fdffeceeefc3608c736f8
esjjim <esjjim@SAG-3VB8PC2.eur.ad.sag>
2020-04-06 13:11:07 +0200

    search by category

--- springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookCategoryRepository.java
+++ springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookCategoryRepository.java
@@ -9,6 +9,7 @@ import in.bushansirgur.onlinebookstore.entity.BookCategory;
 
 // * Customizing Spring Data Rest > Configuring the Rest URL Path
 @RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
+//@CrossOrigin("http://localhost:4200") eliminat amb config.getCorsRegistry()
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
-
+	
 }
--- springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookRepository.java
+++ springboot-online-bookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.bushansirgur.onlinebookstore.entity.Book;
 
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200") eliminat amb config.getCorsRegistry()
 public interface BookRepository extends JpaRepository<Book, Long> {
 	
 	@RestResource(path="categoryid")
